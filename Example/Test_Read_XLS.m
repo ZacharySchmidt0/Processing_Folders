147141f948070db54ac4db428802cc85fdb5dfbc
Zachary Schmidt <zacharyaschmidt@hotmail.ca>
2022-06-20 12:16:38 -0600

    Pulled first row

--- Example/Test_Read_XLS.m
+++ Example/Test_Read_XLS.m
@@ -7,4 +7,19 @@ clear
 %ssds = spreadsheetDatastore("MecE260_UNDERGRAD Grade Dist Form.xlsx")
 %sheetnames(ssds,1)
 filename = "MecE260_UNDERGRAD Grade Dist Form.xlsx";
-T = readtable(filename,'Sheet','SECOND YEAR')
+T = readtable(filename,'Sheet','SECOND YEAR');
+
+data = struct;
+valueFlag = 0;
+
+for i = 1:size(T.Properties.VariableNames, 2)
+    if ~strcmp(extractBetween(T.Properties.VariableNames(i), 1, 3), 'Var')
+        if valueFlag == 0
+            field = T.Properties.VariableNames(i);
+            valueFlag = 1;
+        elseif valueFlag == 1
+            [data(:).(field{1})] = T.Properties.VariableNames(i);
+            valueFlag = 0;
+        end     
+    end
+end
