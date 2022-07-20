d02c3c894ae91efe01dccced1053379fd130c744
jaskim9824 <jaskim9824@gmail.com>
2022-07-20 14:47:11 -0600

    implemented gpaforcourse

--- Example/plotGPAForCourse.m
+++ Example/plotGPAForCourse.m
@@ -1,3 +1,15 @@
-function plotGPAForCourse(classData, course)
-    fieldNms = fieldnames(classData)
-    
+function average = plotGPAForCourse(classData, course)
+    fieldNms = fieldnames(classData);
+    GPtotal = 0
+    studentTotal = 0
+    for i=1:numel(fieldNms)
+        structArray = classData.(fieldNms{i});
+        for j=1:numel(structArray)
+            struct = structArray(j);
+            if (struct.course == course)
+                GPtotal = GPtotal + (struct.classGPA * struct.number_of_stundents_in_class);
+                studentTotal = studentTotal + struct.number_of_stundents_in_class
+            end
+        end
+        average = GPtotal / studentTotal
+    end
