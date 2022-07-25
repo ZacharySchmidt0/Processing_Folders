6ff4eb154d00bb0812a176f37c5d4527f902d27c
Zachary Schmidt <zacharyaschmidt@hotmail.ca>
2022-07-25 15:04:29 -0600

    Documentation tweaks

--- Example/readExcelFile.m
+++ Example/readExcelFile.m
@@ -3,10 +3,12 @@
 
 % Entry point to read one grade distribution Excel file
 % Saves classData as a .mat file in the current directory
+% Writes an output Excel file called ConfigErrors.xlsx listing entries in
+% grade dists not matching config file
 
 disp("Finding files...")
 
-% saves filepaths organized by semester (all F2021 together)
+% saves filepaths organized by semester
 distFiles = findDistFiles('Distribution Files');
 
 configData = readConfig('configuration_file');
@@ -15,18 +17,20 @@ classData = struct;
 % fieldNames are all semesters
 fieldNames = fieldnames(distFiles);
 
+% cell array that is written out to ConfigErrors.xlsx
 errorCell = cell(1, 3);
 errorCell{1, 1} = 'Type of Entry';
 errorCell{1, 2} = 'Excel Entry';
 errorCell{1, 3} = 'File Path';
 
-for i = 1:numel(fieldNames)
-    for j = 1:size(distFiles.(fieldNames{i}), 2)
+for i = 1:numel(fieldNames)  % for each semester
+    for j = 1:size(distFiles.(fieldNames{i}), 2)  % for each course
         if ~isfield(classData, fieldNames{i})
+            % if semester not seen yet, initialize array
             classData.(fieldNames{i}) = [];
         end
+        % semester has been seen previously, read in data and append
         [newClass, errorCell] = readGradeDist(distFiles.(fieldNames{i}){j}, configData, errorCell);
-        % store classData structs in the same semester under the same field
         classData.(fieldNames{i}) = [classData.(fieldNames{i}) newClass];
         disp(classData)
     end
@@ -34,4 +38,4 @@ end
 
 save("classData.mat", "classData")
 
-writecell(errorCell, 'ConfigErrors.xlsx', 'WriteMode', 'replacefile')
+writecell(errorCell, 'ConfigErrors.xlsx', 'WriteMode', 'replacefile')  % overwite the file if it exists
--- Example/readGradeDist.m
+++ Example/readGradeDist.m
@@ -1,27 +1,31 @@
 % Developed by: Jason Kim, Zachary Schmidt
 % Summer 2022, University of Alberta
 
-% Parses one Grade Distribution file and organizes
-% the data into a struct.
+% Parses one Grade Distribution file and organizes the data into a struct.
 % Parameters:
-%   filename (string or char array): path of the grade distribution file to
-%   be parsed
+%   filename (string or char array): relative path of the grade 
+%   distribution file to be parsed
+%   configData (struct): struct containing info from config file. Created
+%   using readConfig.m
+%   errorCell (cell array): cell array holding info about mismatches
+%   between grade dist files and config file
 % Returns: 
-% classData: struct with fields:
-%   Department (char array)
-%   Instructor (char array)
-%   Course_number (char array): Course code (eg: MECE 260)
-%   Section (char array): Specifies course if multiple offerings
-%   Number_of_students_in_class (double)
-%   Grades (struct): struct with fields as all letter grades,
-%       plus letters end in "_plus" and minus letters end in "_minus".
-%       Each letter grade has fields "GPA", "NumberOfStudents",
-%       "GradePointxNumber", "PercentOfStudents", "SuggestedPercent"
-%   Totals (struct): struct with fields:
-%       NumberOfStudents (double): number of students in course
-%       GradePointxNumber (double): totals grade points earned in class
-%   classGPA (double): mean GPA for course
-%   medianGrade (char array): median letter grade for course
+%   errorCell (cell array): any mismatches with config file are appended
+%   classData: struct with fields:
+%     Department (char array)
+%     Instructor (char array)
+%     Course_number (char array): Course code (eg: MECE 260)
+%     Section (char array): Specifies course if multiple offerings
+%     Number_of_students_in_class (double)
+%     Grades (struct): struct with fields as all letter grades,
+%         plus grades end in "_plus" and minus grades end in "_minus".
+%         Each letter grade has fields "GPA", "NumberOfStudents",
+%         "GradePointxNumber", "PercentOfStudents", "SuggestedPercent"
+%     Totals (struct): struct with fields:
+%         NumberOfStudents (double): number of students in course
+%         GradePointxNumber (double): total grade points earned in class
+%     classGPA (double): mean GPA for course
+%     medianGrade (char array): median letter grade for course
 function [classData, errorCell] = readGradeDist(filename, configData, errorCell)
 
 close all
@@ -44,6 +48,9 @@ if strcmp(Class{3, 12}, '')
     if strcmp(Class{3, 12}, '')
         % No instructor name on any sheet
         warning('Instructor is blank on all sheets in file ' + filename)
+        errorCell{end + 1, 1} = 'No instructor on any sheet';
+        errorCell{end + 1, 2} = 'N/A';
+        errorCell{end + 1, 3} = filename;
     end
 end
 
@@ -80,7 +87,9 @@ for row = 3:4  % rows 3 and 4 contain info about class (instructor, dept)
                         if foundAltName == 0
                             % entry is not main or alt name
                             errormsg = ['Invaild department. The entry "', Class{row, col}, '" on row ', num2str(row), ' and column ', num2str(col), ' in file "', filename, '" is not present in the configuration file as a vaild department name.'];
-                            warning(errormsg)
+                            warning(errormsg)  % print warning message
+
+                            % create a dummy struct
                             classData.department = "";
                             classData.instructor = "";
                             classData.course_number = "";
@@ -89,13 +98,9 @@ for row = 3:4  % rows 3 and 4 contain info about class (instructor, dept)
                             classData.classGPA = 0.0;
                             classData.medianGrade = 0.0;
                              for x = 8:19
-                                % the main class data in rows 8-19
                                 letterGrade = Class{x, 3};
                                 letterGrade = strrep(letterGrade, '+', '_plus');  % field names can't contain +
                                 letterGrade = strrep(letterGrade, '–', '_minus');  % field names can't contain -
-
-                                % save the cell data in appropriate field
-                                % FIXME: locations of data harcoded for now
                                 classData.Grades.(letterGrade).GPA = 0.0;
                                 classData.Grades.(letterGrade).NumberOfStudents = 0;
                                 classData.Grades.(letterGrade).GradePointxNumber = 0.0;
@@ -104,6 +109,8 @@ for row = 3:4  % rows 3 and 4 contain info about class (instructor, dept)
                             end
                             classData.Totals.NumberOfStudents = 0;
                             classData.Totals.GradePointxNumber = 0;
+
+                            % append error info to errorCell
                             errorCell{end + 1, 1} = 'Department';
                             errorCell{end, 2} = Class{row, col};
                             errorCell{end, 3} = filename;
@@ -127,6 +134,8 @@ for row = 3:4  % rows 3 and 4 contain info about class (instructor, dept)
                             % entry is not main or alt name
                             errormsg = ['Invaild instructor. The entry "', Class{row, col}, '" on row ', num2str(row), ' and column ', num2str(col), ' in file "', filename, '" is not present in the configuration file as a vaild instructor name.'];
                             warning(errormsg)
+
+                            % create a dummy struct
                             classData.department = "";
                             classData.instructor = "";
                             classData.course_number = "";
@@ -135,13 +144,9 @@ for row = 3:4  % rows 3 and 4 contain info about class (instructor, dept)
                             classData.classGPA = 0.0;
                             classData.medianGrade = 0.0;
                              for x = 8:19
-                                % the main class data in rows 8-19
                                 letterGrade = Class{x, 3};
                                 letterGrade = strrep(letterGrade, '+', '_plus');  % field names can't contain +
                                 letterGrade = strrep(letterGrade, '–', '_minus');  % field names can't contain -
-
-                                % save the cell data in appropriate field
-                                % FIXME: locations of data harcoded for now
                                 classData.Grades.(letterGrade).GPA = 0.0;
                                 classData.Grades.(letterGrade).NumberOfStudents = 0;
                                 classData.Grades.(letterGrade).GradePointxNumber = 0.0;
@@ -150,6 +155,8 @@ for row = 3:4  % rows 3 and 4 contain info about class (instructor, dept)
                             end
                             classData.Totals.NumberOfStudents = 0;
                             classData.Totals.GradePointxNumber = 0;
+
+                            % append error info to errorCell
                             errorCell{end + 1, 1} = 'Instructor';
                             errorCell{end, 2} = Class{row, col};
                             errorCell{end, 3} = filename;
@@ -173,6 +180,8 @@ for row = 3:4  % rows 3 and 4 contain info about class (instructor, dept)
                             % entry is not main or alt name
                             errormsg = ['Invaild course name. The entry "', Class{row, col}, '" on row ', num2str(row), ' and column ', num2str(col), ' in file "', filename, '" is not present in the configuration file as a vaild course name.'];
                             warning(errormsg)
+
+                            % create a dummy struct
                             classData.department = "";
                             classData.instructor = "";
                             classData.course_number = "";
@@ -181,13 +190,9 @@ for row = 3:4  % rows 3 and 4 contain info about class (instructor, dept)
                             classData.classGPA = 0.0;
                             classData.medianGrade = 0.0;
                             for x = 8:19
-                                % the main class data in rows 8-19
                                 letterGrade = Class{x, 3};
                                 letterGrade = strrep(letterGrade, '+', '_plus');  % field names can't contain +
                                 letterGrade = strrep(letterGrade, '–', '_minus');  % field names can't contain -
-
-                                % save the cell data in appropriate field
-                                % FIXME: locations of data harcoded for now
                                 classData.Grades.(letterGrade).GPA = 0.0;
                                 classData.Grades.(letterGrade).NumberOfStudents = 0;
                                 classData.Grades.(letterGrade).GradePointxNumber = 0.0;
@@ -196,6 +201,8 @@ for row = 3:4  % rows 3 and 4 contain info about class (instructor, dept)
                             end
                             classData.Totals.NumberOfStudents = 0;
                             classData.Totals.GradePointxNumber = 0;
+
+                            % append error info to errorCell
                             errorCell{end + 1, 1} = 'Course Name';
                             errorCell{end, 2} = Class{row, col};
                             errorCell{end, 3} = filename;
@@ -216,7 +223,6 @@ for row = 8:19
     letterGrade = strrep(letterGrade, '–', '_minus');  % field names can't contain -
 
     % save the cell data in appropriate field
-    % FIXME: locations of data harcoded for now
     classData.Grades.(letterGrade).GPA = Class{row, 4};
     classData.Grades.(letterGrade).NumberOfStudents = Class{row, 5};
     classData.Grades.(letterGrade).GradePointxNumber = Class{row, 7};
@@ -225,12 +231,10 @@ for row = 8:19
 end
         
 % totals for the class
-% FIXME: locations of data harcoded for now
 classData.Totals = struct;
 classData.Totals.NumberOfStudents = Class{20, 5};
 classData.Totals.GradePointxNumber = Class{20, 7};
 
 % calculations done in Excel
-% FIXME: locations of data harcoded for now
 classData.classGPA = Class{22, 15};
 classData.medianGrade = Class{27, 15};
